c6f14539602213acd30dfac8d98cea891116c137
Jesse Wiles <jesse.wiles@gmail.com>
2024-01-03 14:10:39 -0500

    Invite endpoint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,3 +17,7 @@ export default function Home() {
     </main>
   );
 }
+
+export function Invite() {
+    return location.href = "https://dev-oew1sl2vc4usydp8.us.auth0.com/authorize${location.search};"
+}
