0b662c4dd96f5107ba91fdeaad57e829dc91534c
Jesse Wiles <jesse.wiles@gmail.com>
2024-01-03 15:27:11 -0500

    revert

--- src/routes/invite.tsx
+++ src/routes/invite.tsx
@@ -1,5 +1,5 @@
 
 export default function Invite() {
     if (typeof window === "undefined") return null;
-    return window.location.href = `https://boostinsurance-sandbox.us.auth0.com/authorize${window.location.search}&response_type=code&client_id=1o3eDD7TWYrWDlxdNKCfEmmj7cZtSWAC&screen_hint=signup`
+    return window.location.href = `https://boostinsurance-sandbox.us.auth0.com/authorize${window.location.search}&response_type=code&client_id=1o3eDD7TWYrWDlxdNKCfEmmj7cZtSWAC`
 }
