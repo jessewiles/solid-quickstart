054f288a0157c917e9b538685107a7489da93f0f
Jesse Wiles <jesse.wiles@gmail.com>
2024-01-03 14:31:59 -0500

    Fix location 3

--- src/routes/invite.tsx
+++ src/routes/invite.tsx
@@ -1,4 +1,5 @@
 
 export default function Invite() {
+    if (typeof window === "undefined") return null;
     return window.location.href = "https://dev-oew1sl2vc4usydp8.us.auth0.com/authorize${location.search};"
 }
