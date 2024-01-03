0528023d49a5035f5fad936957b99306c22d1bbe
Jesse Wiles <jesse.wiles@gmail.com>
2024-01-03 14:44:42 -0500

    Fix location 9

--- src/routes/invite.tsx
+++ src/routes/invite.tsx
@@ -1,5 +1,5 @@
 
 export default function Invite() {
     if (typeof window === "undefined") return null;
-    return window.location.href = `https://boostinsurance-sandbox.us.auth0.com/authorize${window.location.search}`
+    return window.location.href = `https://boostinsurance-sandbox.us.auth0.com/authorize${window.location.search}&response_type=code`
 }
